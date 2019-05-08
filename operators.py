2005fd2e58cd4ade5410c23b1a3b710b026dadd3
Manik <manikraina1096@gmail.com>
2019-05-08 21:27:03 +0530

    operators.py
    defining a function 'solve' to calculate the total meal price after adding tax and tip percentage cost

---
+++ operators.py
@@ -0,0 +1,13 @@
+def solve(meal_cost, tip_percent, tax_percent):
+    tip = meal_cost * (tip_percent/100)
+    tax = meal_cost * (tax_percent/100)
+    meal_cost = meal_cost + tip + tax
+    return round(meal_cost)
+if __name__ == '__main__':
+    meal_cost = float(input())
+
+    tip_percent = int(input())
+
+    tax_percent = int(input())
+
+    print(solve(meal_cost, tip_percent, tax_percent))
